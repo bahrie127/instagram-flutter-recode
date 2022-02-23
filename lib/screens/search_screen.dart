21fc4b08cf146f7d160b49685b1044d0cf578cb4
bahri <bahri@atmatech.net>
2022-02-23 08:00:13 +0700

    search user future builder

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,72 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter_recode/utils/colors.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController _searchController = TextEditingController();
+  bool _isShowUser = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _searchController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: TextFormField(
+          decoration: const InputDecoration(
+            labelText: 'Search for a user',
+          ),
+          controller: _searchController,
+          onFieldSubmitted: (String _) {
+            setState(() {
+              _isShowUser = true;
+            });
+          },
+        ),
+      ),
+      body: _isShowUser
+          ? FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('users')
+                  .where('username',
+                      isGreaterThanOrEqualTo: _searchController.text)
+                  .get(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: (snapshot.data! as dynamic).docs.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            (snapshot.data! as dynamic).docs[index]
+                                ['photoUrl']),
+                      ),
+                      title: Text(
+                        (snapshot.data! as dynamic).docs[index]['username'],
+                      ),
+                    );
+                  },
+                );
+              },
+            )
+          : Text('Posts'),
+    );
+  }
+}
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter_recode/screens/feed_screen.dart';
 import 'package:instagram_flutter_recode/screens/add_post_screen.dart';
+import 'package:instagram_flutter_recode/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Center(
-    child: Text('Search'),
-  ),
+  SearchScreen(),
   AddPostScreen(),
   Center(
     child: Text('notif'),
